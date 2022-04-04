ab09715d18913b542916195186d71b11c94e07cf
Aissonw210 <aissonw210@gmail.com>
2022-04-04 09:42:42 -0400

    Save Game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // https://api.rawg.io/api/platforms?key=f9ffe099be0a466eb8f7ff46f77e5a20
 function getApi() {
     var requestUrl = 'https://api.rawg.io/api/platforms?key=f9ffe099be0a466eb8f7ff46f77e5a20';  
-
+    var requestUrl= 'https://api.rawg.io/api/platforms?key=f9ffe099be0a466eb8f7ff46f77e5a20';
     fetch(requestUrl)
       .then(function (response) {
         console.log(response);
